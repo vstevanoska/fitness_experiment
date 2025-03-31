a8161e0e66e8d44b1ed7b4aa0fc316967211583f
viktorija <stevanoskaviktorija@gmail.com>
2025-03-31 16:42:11 +0200

    Reading from every file

--- analysis.py
+++ analysis.py
@@ -7,17 +7,21 @@ if __name__ == '__main__':
 
     user = input("Please input for which user you need analysis: ")
 
+    data_path = "C:\\Users\\Viktorija\\Desktop\\VR\\RV1\\git\\FitnessExperimentServer\\FitnessExperimentServer\\data"
+
     if user == "*":
-        print("Analysis for all users!")
+        
+        for i in os.listdir(data_path):
+            with open((data_path + "\\" + i), "r") as json_file:
+                files.append(json.load(json_file))
         
     else:
         
-        data_path = "C:\\Users\\Viktorija\\Desktop\\VR\\RV1\\git\\FitnessExperimentServer\\FitnessExperimentServer\\data"
-
         for i in os.listdir(data_path):
             if (os.path.isfile(os.path.join(data_path, i)) and user in i):
                 
                 with open((data_path + "\\" + i), "r") as json_file:
                     files.append(json.load(json_file))
 
-        print(files[0]["accMeasurements"][0]["timestamp"])
+    for i in files:
+        print("User: ", i["user"])
